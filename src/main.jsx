bfb3ed5542ffc109f304ce0738fa4a6592388896
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-06-10 12:44:23 +0530

    vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -6,27 +6,10 @@ import App from "./App";
 import { store } from "./app/store";
 import "./index.css";
 
-const Home = lazy(() => import("./routes/Home"));
-
-const router = createBrowserRouter([
-  {
-    path: "/weather-app-vite",
-    element: (
-      <Suspense fallback={<div className="hidden">Loading...</div>}>
-        <App />
-      </Suspense>
-    ),
-  },
-  {
-    path: "*",
-    element: <div>Not Found</div>,
-  },
-]);
-
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <App />
     </Provider>
   </React.StrictMode>
 );
