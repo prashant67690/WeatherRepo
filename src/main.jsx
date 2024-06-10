68bf488bd0dc3f8abca00a0c3249f0bf9d41ac2d
prashant kumar <80661803+prashant67690@users.noreply.github.com>
2024-06-10 12:48:52 +0530

    vercel.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./app/store";
